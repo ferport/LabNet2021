d79479da89f87f3a9bbd1923a9dd42d9d9076d2e
ferport <portillofernandobek@gmail.com>
2021-05-07 10:41:30 -0300

    Resubiendo proyecyo

---
+++ TP-EF - copia/TPEntityFramework/TPEntityFramework.Logic/Validations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TPEntityFramework.Logic
+{
+    public static class Validations
+    {
+        public static int NumberValidation(string num)
+        {
+            int numParse;
+            if (int.TryParse(num, out numParse))
+            {
+                return numParse;
+            }
+
+            return 0;
+        }
+
+        public static int ConfirmValidation(string confirm)
+        {
+            if (confirm != "s" && confirm != "n")
+            {
+                return 1;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+    }
+}
